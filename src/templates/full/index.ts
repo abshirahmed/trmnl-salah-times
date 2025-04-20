63048eea9be31518d0830e112ffc948fba3394a9
Abshir <abshir@joinpopp.com>
2025-04-20 05:29:20 +0100

    feat(preview): update preview server port and enhance template structure

--- src/templates/full/index.ts
+++ src/templates/full/index.ts
@@ -6,156 +6,117 @@ export const fullTemplate = `
 <div class="environment trmnl">
   <div class="screen">
     <div class="view view--full">
-      <div class="layout padding--large">
-        <!-- Header Section with Current Time and Date -->
-        <div class="flex flex--col flex--align-center gap--medium margin-bottom--medium">
-          <div class="flex flex--col flex--align-center gap--small">
-            <div class="value value--xlarge value--tnums format-value">{{ IDX_0.enhancedData.currentTime }}</div>
-            <div class="flex flex--col flex--align-center gap--small">
-              <div class="description text--center">{{ IDX_0.enhancedData.gregorianDate }}</div>
-              <div class="description description--small text--center">Last Updated: {{ IDX_0.enhancedData.lastSyncTime }}</div>
+      <!-- Main Layout -->
+      <div class="layout layout--col">
+        <!-- Gregorian and Hijri Dates -->
+        <div class="stretch-x grid grid-col-2 border--h-3">
+          <div class="row row--start">
+            <div>
+              <span class="title">Gregorian Date</span>
+              <span class="value value--xxsmall">{{ IDX_0.enhancedData.gregorianDate }}</span>
+           </div>
+          </div>
+          <div class="row row--end">
+            <div>
+              <span class="title">Hijri Date</span>
+              <span class="value value--xxsmall">{{ IDX_0.enhancedData.hijriDateFormatted }}</span>
             </div>
           </div>
         </div>
-
-        <!-- Next Prayer Information - Prominent Display -->
-        <div class="item background--dark border radius margin-bottom--large">
-          <div class="content padding--large">
-            <div class="flex flex--col flex--align-center gap--medium">
-              <span class="label label--large">Next Prayer</span>
-              <div class="flex flex--col flex--align-center gap--medium">
-                <span class="title title--large fit-value" data-min-size="32" data-max-size="48">{{ IDX_0.enhancedData.nextPrayer }}</span>
-                <span class="value value--large value--tnums format-value">{{ IDX_0.enhancedData.nextPrayerTime }}</span>
+        <!-- Next Prayer Info -->
+        <div class="stretch-x grid  bg--gray-6">
+          <div class="col pt--2 pl--1">
+            <div>
+              <span class="title title--small">Next</span>
+            </div>
+            <div class="grid grid-col-2">
+              <div class="row start">
+                <span class="value">{{ IDX_0.enhancedData.nextPrayer | upcase }}</span>
               </div>
-              <div class="flex flex--align-center gap--medium">
-                <span class="value value--large format-value">{{ IDX_0.enhancedData.timeUntilNextPrayer.hours }}</span>
-                <span class="description">hours</span>
-                <span class="value value--large format-value">{{ IDX_0.enhancedData.timeUntilNextPrayer.minutes }}</span>
-                <span class="description">minutes</span>
+            <div class="row row--end">
+                <span class="value">{{ IDX_0.enhancedData.nextPrayerTime }}</span>
               </div>
             </div>
           </div>
         </div>
-
-        <!-- Prayer Times Section -->
-        <div class="flex flex--col gap--medium">
+        <!-- Prayer Times -->
+        <div class="stretch-x stretch-y">
+        <div class="flex flex--col gap--space-between h-full">
+        <!-- Header -->
+          <div class="grid grid-col-2 border--h-1">
+            <div class="row row--start">
+              <span class="label">Prayer</span>
+            </div>
+            <div class="row row--end">
+              <span class="label">Time</span>
+            </div>
+          </div>
           <!-- Fajr -->
-          <div class="item {% if IDX_0.enhancedData.currentPrayer == 'Fajr' %}background--dark{% else %}background--light{% endif %} border radius">
-            <div class="content padding--medium">
-              <div class="flex flex--between flex--align-center">
-                <div class="flex flex--col gap--small">
-                  <span class="label label--large">Fajr Prayer</span>
-                  {% if IDX_0.enhancedData.currentPrayer == 'Fajr' %}
-                    <span class="description description--small">Current Prayer</span>
-                  {% endif %}
-                </div>
-                <span class="value value--large value--tnums format-value">{{ IDX_0.data.timings.Fajr | slice: 0, 5 }}</span>
-              </div>
+          <div class="grid grid-col-2 border--h-3">
+            <div class="row row--start">
+              <span class="title">Fajr</span>
+            </div>
+            <div class="row row--end">
+              <span class="title">{{ IDX_0.data.timings.Fajr | slice: 0, 5 }}</span>
             </div>
           </div>
-
           <!-- Sunrise -->
-          <div class="item background--light border radius">
-            <div class="content padding--medium">
-              <div class="flex flex--between flex--align-center">
-                <div class="flex flex--col gap--small">
-                  <span class="label label--large">Sunrise</span>
-                  <span class="description description--small">End of Fajr Time</span>
-                </div>
-                <span class="value value--large value--tnums format-value">{{ IDX_0.data.timings.Sunrise | slice: 0, 5 }}</span>
-              </div>
+          <div class="grid grid-col-2 border--h-3">
+            <div class="row row--start">
+              <span class="title">Sunrise</span>
             </div>
-          </div>
-
+            <div class="row row--end">
+              <span class="title">{{ IDX_0.data.timings.Sunrise | slice: 0, 5 }}</span>
+            </div>
+        </div>
           <!-- Dhuhr -->
-          <div class="item {% if IDX_0.enhancedData.currentPrayer == 'Dhuhr' %}background--dark{% else %}background--light{% endif %} border radius">
-            <div class="content padding--medium">
-              <div class="flex flex--between flex--align-center">
-                <div class="flex flex--col gap--small">
-                  <span class="label label--large">Dhuhr Prayer</span>
-                  {% if IDX_0.enhancedData.currentPrayer == 'Dhuhr' %}
-                    <span class="description description--small">Current Prayer</span>
-                  {% endif %}
-                </div>
-                <span class="value value--large value--tnums format-value">{{ IDX_0.data.timings.Dhuhr | slice: 0, 5 }}</span>
-              </div>
+          <div class="grid grid-col-2 border--h-3">
+            <div class="row row--start">
+              <span class="title">Dhuhr</span>
+            </div>
+            <div class="row row--end">
+              <span class="title">{{ IDX_0.data.timings.Dhuhr | slice: 0, 5 }}</span>
             </div>
           </div>
-
           <!-- Asr -->
-          <div class="item {% if IDX_0.enhancedData.currentPrayer == 'Asr' %}background--dark{% else %}background--light{% endif %} border radius">
-            <div class="content padding--medium">
-              <div class="flex flex--between flex--align-center">
-                <div class="flex flex--col gap--small">
-                  <span class="label label--large">Asr Prayer</span>
-                  {% if IDX_0.enhancedData.currentPrayer == 'Asr' %}
-                    <span class="description description--small">Current Prayer</span>
-                  {% endif %}
-                </div>
-                <span class="value value--large value--tnums format-value">{{ IDX_0.data.timings.Asr | slice: 0, 5 }}</span>
-              </div>
+          <div class="grid grid-col-2 border--h-3">
+            <div class="row row--start">
+              <span class="title">Asr</span>
+            </div>
+            <div class="row row--end">
+              <span class="title">{{ IDX_0.data.timings.Asr | slice: 0, 5 }}</span>
             </div>
           </div>
-
           <!-- Maghrib -->
-          <div class="item {% if IDX_0.enhancedData.currentPrayer == 'Maghrib' %}background--dark{% else %}background--light{% endif %} border radius">
-            <div class="content padding--medium">
-              <div class="flex flex--between flex--align-center">
-                <div class="flex flex--col gap--small">
-                  <span class="label label--large">Maghrib Prayer</span>
-                  {% if IDX_0.enhancedData.currentPrayer == 'Maghrib' %}
-                    <span class="description description--small">Current Prayer</span>
-                  {% endif %}
-                </div>
-                <span class="value value--large value--tnums format-value">{{ IDX_0.data.timings.Maghrib | slice: 0, 5 }}</span>
-              </div>
+          <div class="grid grid-col-2 border--h-3">
+            <div class="row row--start">
+              <span class="title">Maghrib</span>
+            </div>
+            <div class="row row--end">
+              <span class="title">{{ IDX_0.data.timings.Maghrib | slice: 0, 5 }}</span>
             </div>
           </div>
-
           <!-- Isha -->
-          <div class="item {% if IDX_0.enhancedData.currentPrayer == 'Isha' %}background--dark{% else %}background--light{% endif %} border radius">
-            <div class="content padding--medium">
-              <div class="flex flex--between flex--align-center">
-                <div class="flex flex--col gap--small">
-                  <span class="label label--large">Isha Prayer</span>
-                  {% if IDX_0.enhancedData.currentPrayer == 'Isha' %}
-                    <span class="description description--small">Current Prayer</span>
-                  {% endif %}
-                </div>
-                <span class="value value--large value--tnums format-value">{{ IDX_0.data.timings.Isha | slice: 0, 5 }}</span>
-              </div>
+          <div class="grid grid-col-2 border--h-3">
+            <div class="row row--start">
+              <span class="title">Isha</span>
+            </div>
+            <div class="row row--end">
+              <span class="title">{{ IDX_0.data.timings.Isha | slice: 0, 5 }}</span>
             </div>
           </div>
         </div>
-
-        <!-- Additional Information Section -->
-        <div class="flex flex--col gap--medium margin-top--medium">
-          <!-- Islamic Date -->
-          <div class="item background--light border radius">
-            <div class="content padding--medium">
-              <div class="flex flex--col flex--align-center gap--medium">
-                <span class="label">Islamic Date</span>
-                <span class="value value--large">{{ IDX_0.data.date.hijri.weekday.en }}, {{ IDX_0.data.date.hijri.day }} {{ IDX_0.data.date.hijri.month.en }} {{ IDX_0.data.date.hijri.year }} {{ IDX_0.data.date.hijri.designation.abbreviated }}</span>
-                {% if IDX_0.data.date.hijri.holidays.length > 0 %}
-                  <span class="description description--small">{{ IDX_0.data.date.hijri.holidays[0] }}</span>
-                {% endif %}
-              </div>
-            </div>
-          </div>
-
-          <!-- Calculation Method -->
-          <div class="item background--light border radius">
-            <div class="content padding--medium">
-              <div class="flex flex--col flex--align-center gap--medium">
-                <span class="label">Calculation Method</span>
-                <span class="value value--medium">{{ IDX_0.data.meta.method.name }}</span>
-                <span class="description description--small">Fajr Angle: {{ IDX_0.data.meta.method.params.Fajr }}° • Isha Angle: {{ IDX_0.data.meta.method.params.Isha }}°</span>
-              </div>
-            </div>
-          </div>
+        </div>
+        <!-- Metadata in 1 row -->
+        <div class="stretch-x">
+        <div class="flex gap--space-between w-full">
+            <!-- Calculation Method -->
+              <span class="description">Calculation Method: {{ IDX_0.data.meta.method.name }}</span>
+            <!-- Last Sync -->
+            <span class="description">Last Sync: {{ IDX_0.enhancedData.lastSyncTime }}</span>
+        </div>
         </div>
       </div>
-
       <!-- Title Bar -->
       <div class="title_bar">
         <img class="image" src="https://usetrmnl.com/images/plugins/private_plugin.svg" />
